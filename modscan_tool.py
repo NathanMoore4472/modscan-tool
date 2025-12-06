63a630755890edbdfd33efe2b75e247c256617e9
Nathan Moore <nathanmoore4472@gmail.com>
2025-12-06 20:09:44 +0100

    Fix version number to 1.3.3

--- modscan_tool.py
+++ modscan_tool.py
@@ -61,7 +61,7 @@ class WorkerSignals(QObject):
 class ModbusScannerGUI(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.app_version = "1.2.0"
+        self.app_version = "1.3.3"
         self.setWindowTitle("ModScan Tool")
         self.setGeometry(100, 100, 1050, 750)
 
